e0a9df05e2d7e6884594cb644a1336dac7cba9ad
John Rinehart <john.rinehart@ardanlabs.com>
2021-05-16 09:18:03 +0300

    feat[server]: support rotating an image

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/johnrichardrinehart/Neuralink-Takehome
 go 1.15
 
 require (
+	github.com/BurntSushi/graphics-go v0.0.0-20160129215708-b43f31a4a966 // indirect
 	google.golang.org/grpc v1.37.1
 	google.golang.org/protobuf v1.25.0
 )
--- server/pkg/server.go
+++ server/pkg/server.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"bytes"
 	"context"
+	"errors"
+	"image"
 	"log"
+	"math"
 
+	"github.com/BurntSushi/graphics-go/graphics"
 	pb "github.com/johnrichardrinehart/Neuralink-Takehome/proto"
 )
 
@@ -13,10 +18,29 @@ type Server struct {
 	Debug bool
 }
 
+// RotateImage accepts a request to rotate an image and, if it's of a valid type (PNG/JPG/GIF)
+// rotates it by the requested angle and returns it
+// Failure to decode will return an error
 func (s Server) RotateImage(ctx context.Context, req *pb.NLImageRotateRequest) (*pb.NLImage, error) {
 	if s.Debug {
 		log.Printf("received request to rotate an image: %v degrees", 90*req.Rotation)
 	}
+
+	rdr := bytes.NewBuffer(req.Image.Data)
+	img, _, err := image.Decode(rdr)
+
+	if err != nil {
+		return nil, errors.New("failed to decode the image - invalid format")
+	}
+
+	if req.Rotation == 0 {
+		return req.Image, nil
+	}
+
+	dst := image.NewRGBA(image.Rect(0, 0, int(req.Image.Width), int(req.Image.Height)))
+	graphics.Rotate(dst, img, &graphics.RotateOptions{Angle: (math.Pi / 2) * float64(req.Rotation)})
+
+	req.Image.Data = dst.Pix
 
 	return req.Image, nil
 }
