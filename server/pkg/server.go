407c5b1661969283c2270781518a88177c5ed7fb
John Rinehart <john.rinehart@ardanlabs.com>
2021-05-16 20:26:24 +0300

    fix: improve average algorithm (still seems to be an issue with the colors)

--- server/pkg/server.go
+++ server/pkg/server.go
@@ -80,6 +80,9 @@ func (s Server) RotateImage(ctx context.Context, req *pb.NLImageRotateRequest) (
 }
 
 func (s Server) MeanFilter(ctx context.Context, img *pb.NLImage) (*pb.NLImage, error) {
+	w := int(img.Width)
+	h := int(img.Height)
+
 	if s.Debug {
 		log.Printf("received request to filter an image")
 	}
@@ -94,35 +97,69 @@ func (s Server) MeanFilter(ctx context.Context, img *pb.NLImage) (*pb.NLImage, e
 	}
 
 	var neighbors [][2]int = [][2]int{
-		{-1 * stride, 0},           // bottom
-		{1 * stride, 0},            // top
-		{0, -1 * stride},           // left
-		{0, 1 * stride},            // right
-		{-1 * stride, -1 * stride}, // bottom left
-		{1 * stride, 1 * stride},   // top right
-		{-1 * stride, 1 * stride},  // top left
-		{1 * stride, -1 * stride},  // bottom right
+		{-1, -1}, // top left
+		{0, -1},  // top
+		{1, -1},  // top right
+		{-1, 0},  // left
+		{1, 0},   // right
+		{-1, 1},  // bottom left
+		{0, 1},   // bottom
+		{1, 1},   // bottom right
 	}
 
-	for i := 0; i < len(img.Data); i += 1 {
-		row := i / int(img.Width)
-		col := i % int(img.Width)
-		acc := img.Data[i]
-		var cnt int
-		for _, n := range neighbors {
-			ro := n[0]                                // row offset
-			co := n[1]                                // col offset
-			j := (row-ro)*int(img.Width) + (col + co) // WARNING: cast could break on extremely large widths
-			if j < 0 || j > len(img.Data)-1 {
-				continue
+	mean := make([]byte, len(img.Data))
+
+	// pixel loop
+	for i := 0; i < len(img.Data); i += stride {
+		ix, iy := iToXY(i, stride, w) // position of 'r' in 'rgb'
+
+		// color loop
+		for c := 0; c < stride; c += 1 {
+			acc := img.Data[i+c] // accumulator
+			cnt := 1             // initial weight
+			for _, n := range neighbors {
+				co := n[0] // col displacement
+				ro := n[1] // row displacement
+
+				// neighbor index
+				if ix+co < 0 || ix+co > w-1 {
+					continue
+				}
+				if iy+ro < 0 || iy+ro > h-1 {
+					continue
+				}
+
+				ni := XYToI(ix+co, iy+ro, stride, c, w)
+
+				cnt += 1
+				acc += img.Data[ni]
 			}
-			cnt += 1
-			acc += img.Data[j]
+			// calculate the mean
+			mean[i+c] = byte(int(acc) / cnt)
 		}
-		img.Data[i] = byte(int(acc) / cnt) // mean
+
 	}
+
+	img.Data = mean
 
 	return img, nil
+}
+
+// iToXY takes the index of an element in a row-ordered linear array and outputs the (x,y) coordinate of the pixel
+// within the picture
+// each pixel is assumed to be a stride-dimensional sub-array
+func iToXY(i, stride, width int) (x, y int) {
+	n := i / stride // aggregate by stride length (rgb have the same pixel)
+	x = n % width
+	y = n / width
+	return
+}
+
+// XYtoI converts the (x,y) coordinate of a stride-dimensional pixel and a color "offset"
+// and returns the index in a row-ordered linear array
+func XYToI(x, y, stride, colorOffset, width int) (i int) {
+	i = stride*(x+y*width) + colorOffset
+	return
 }
 
 func validateImage(img *pb.NLImage) error {
