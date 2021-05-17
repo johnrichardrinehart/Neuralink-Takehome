c15f959c1124e843146435d163e43c897346c347
John Rinehart <john.rinehart@ardanlabs.com>
2021-05-17 14:01:45 +0300

    chore[signal]: add signal handling and graceful cleanup to the server

--- server/main.go
+++ server/main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 
 	server "github.com/johnrichardrinehart/Neuralink-Takehome/server/pkg"
 
@@ -23,6 +26,16 @@ func main() {
 	flag.StringVar(&host, "host", "localhost", "default interface on which to listen")
 	flag.BoolVar(&debug, "debug", false, "debug mode (logs some runtime behavior)")
 	flag.Parse()
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		done <- true
+	}()
 
 	if v, err := strconv.Atoi(port); err != nil || v < 0 || v > 1<<16 {
 		log.Fatal("invalid port number specified - must be between 0 and 65535 (0 requests a random port from the OS kernel)")
@@ -43,7 +56,14 @@ func main() {
 		Debug: debug,
 	})
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+	<-done
+	s.GracefulStop()
+	if debug {
+		log.Printf("server gracefully exiting")
 	}
 }
