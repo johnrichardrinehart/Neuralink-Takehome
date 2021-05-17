a4d6f10a52489383a4c8d34d968938592cd6b503
John Rinehart <john.rinehart@ardanlabs.com>
2021-05-17 18:40:04 +0300

    fix[setup]: ensure repeated downloads overwrite the same file

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ function host {
 	apt-get install -y unzip
 
 	export PROTOC_FILENAME="protoc-3.12.1-linux-x86_64.zip"
-	curl -LO "https://github.com/protocolbuffers/protobuf/releases/download/v3.12.1/${PROTOC_FILENAME}"
+	curl -LO "https://github.com/protocolbuffers/protobuf/releases/download/v3.12.1/${PROTOC_FILENAME}" -o $PROTOC_FILENAME
 	unzip $PROTOC_FILENAME -d /usr/local
 	# clean up
 	rm $PROTOC_FILENAME
@@ -16,15 +16,15 @@ function host {
 	export GOBIN=/usr/local/go/bin
 	export PATH=$GOBIN:$PATH
 	export GO_FILENAME="go1.16.4.linux-amd64.tar.gz"
-	wget https://dl.google.com/go/$GO_FILENAME
+	wget -o ./$GO_FILENAME https://dl.google.com/go/$GO_FILENAME
 	tar -xvf go1.16.4.linux-amd64.tar.gz -C /usr/local
 	# clean up
 	rm $GO_FILENAME
-	chown -R $(logname): ~/go
 
 	# install protoc-gen-go* protoc compiler plugins
 	go get google.golang.org/protobuf/cmd/protoc-gen-go@v1.26 
 	go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
+	chown -R $(logname): ~/go
 
 	# compile the proto definition into a Go package
 	protoc --proto_path=./proto \
